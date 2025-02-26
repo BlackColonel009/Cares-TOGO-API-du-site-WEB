e7d3d0dc3406e0dc4a4bba0c9ff1cbb491cbb4a3
BlackColonel009 <didicko009@gmail.com>
2025-02-26 22:51:40 +0000

    New

--- app/database.py
+++ app/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 # from sqlalchemy.orm import declarative_base
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-from config import DATABASE_URL
+from app.config import DATABASE_URL
 import psycopg2
 import os
 
@@ -11,7 +11,18 @@ try:
     engine = create_engine(DATABASE_URL)
     SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
     Base = declarative_base()
+    
+        # Fonction pour obtenir une session de base de données
+    def get_db():
+        db = SessionLocal()
+        try:
+            yield db
+        finally:
+            db.close()
+            
     with engine.connect() as connection:
         print("✅ Connexion réussie à la base de données !")
+
 except Exception as e:
     print(f"❌ Erreur de connexion : {e}")
+
