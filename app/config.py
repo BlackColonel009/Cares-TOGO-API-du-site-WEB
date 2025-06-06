1728bd613fd0679c7fabb445921c0514a5e42e08
BlackColonel009 <didicko009@gmail.com>
2025-06-06 12:14:37 +0000

    cares

--- app/config.py
+++ app/config.py
@@ -5,5 +5,5 @@ load_dotenv()
 
 print(f"📌 DATABASE_URL trouvée ? {os.getenv('DATABASE_URL') is not None}")
 #DATABASE_URL="postgresql://postgres:roland2001@localhost:5433/cares_togo?client_encoding=UTF8"
-DATABASE_URL = "postgresql://cares_togo_user:Px5O08UIUFiaUh091VC3ueoWcuAYITrf@dpg-d00jpupr0fns73ec42n0-a.virginia-postgres.render.com/cares_togo"
+DATABASE_URL = "postgresql://cares_admin:roland2001@31.220.87.142:5432/cares_db"
 print(f"📌 DATABASE_URL: {DATABASE_URL}")
